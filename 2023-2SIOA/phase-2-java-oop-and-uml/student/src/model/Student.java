c53f52ce048dd647a230c419f4ac97821d686e69
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-04-30 18:14:12 -0300

    Create Student class inside model package

---
+++ 2023-2SIOA/phase-2-java-oop-and-uml/student/src/model/Student.java
@@ -0,0 +1,58 @@
+package model;
+
+public class Student {
+    private Integer rm;
+    private String name;
+    private String email;
+    private String address;
+    private String cpf;
+    private String rg;
+
+    public Integer getRm() {
+        return rm;
+    }
+
+    public void setRm(Integer rm) {
+        this.rm = rm;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+}
